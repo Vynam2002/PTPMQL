db03dc8bf77eaa8b6f0f4835c71a649db40e6e2c
Vynam2002 <vithanhnamckpt2002@gmail.com>
2024-09-16 22:30:43 +0700

    commit note

---
+++ W3.css/CSS Comments.cs
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+/* This is a single-line comment */
+p {
+  color: red;
+} 
+</style>
+</head>
+<body>
+
+<p>Vy thành nam </p>
+<p>2021050469</p>
+<p>Vy thành nam</p>
+
+</body>
+</html>
+
