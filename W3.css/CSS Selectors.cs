3295ed6c5732ecc48ba7a4eaf1a86d8930d1de11
Vynam2002 <vithanhnamckpt2002@gmail.com>
2024-09-16 22:26:55 +0700

    commit note

---
+++ W3.css/CSS Selectors.cs
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+p {
+  text-align: center;
+  color: red;
+} 
+</style>
+</head>
+<body>
+
+<p>2021050469</p>
+<p id="para1">Vy thành nam </p>
+<p>Vy thành Nam </p>
+
+</body>
+</html>
