1b7c85e76439908392b6cb44004f0606b691b6c9
Vynam2002 <vithanhnamckpt2002@gmail.com>
2024-09-16 22:52:15 +0700

    Commit note

---
+++ W3.css/CSS Border Style.cs
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+p.dotted {border-style: dotted;}
+p.dashed {border-style: dashed;}
+p.solid {border-style: solid;}
+p.double {border-style: double;}
+p.groove {border-style: groove;}
+p.ridge {border-style: ridge;}
+p.inset {border-style: inset;}
+p.outset {border-style: outset;}
+p.none {border-style: none;}
+p.hidden {border-style: hidden;}
+p.mix {border-style: dotted dashed solid double;}
+</style>
+</head>
+<body>
+
+<h2>Vy thanh nam</h2>
+<p>2021050469</p>
+
+<p class="dotted">Vy thanh nam </p>
+<p class="dashed">2021050469</p>
+<p class="solid">Vy thanh nam </p>
+<p class="double">Vy thanh nam </p>
+<p class="groove">Vy thanh nam </p>
+<p class="ridge">Vy thanh nam </p>
+<p class="inset">Vy thanh nam </p>
+<p class="outset">Vy thanh nam </p>
+<p class="none">Vy thanh nam .</p>
+<p class="hidden">Vy thanh nam </p>
+<p class="mix">Vy thanh nam </p>
+
+</body>
+</html>
+
+
