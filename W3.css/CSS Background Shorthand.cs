7d7e09f174495ac25adefe9b0716bf6746e58b2e
Vynam2002 <vithanhnamckpt2002@gmail.com>
2024-09-16 22:49:48 +0700

    commit note

---
+++ W3.css/CSS Background Shorthand.cs
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+  background: #ffffff url("img_tree.png") no-repeat right top;
+  margin-right: 200px;
+}
+</style>
+</head>
+<body>
+
+<h1>Vy Thành Nam </h1>
+
+<p>2021050469</p>
+
+<p>2021050469</p>
+
+<p>Vy Thành Nam </p>
+
+</body>
+</html>
+
+
