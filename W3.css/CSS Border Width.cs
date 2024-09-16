41c2d18d3345e87cbcc9617059f794ac30b5eb1e
Vynam2002 <vithanhnamckpt2002@gmail.com>
2024-09-16 22:53:59 +0700

    commit note

---
+++ W3.css/CSS Border Width.cs
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+p.one {
+  border-style: solid;
+  border-width: 5px;
+}
+
+p.two {
+  border-style: solid;
+  border-width: medium;
+}
+
+p.three {
+  border-style: dotted;
+  border-width: 2px;
+}
+
+p.four {
+  border-style: dotted;
+  border-width: thick;
+}
+
+p.five {
+  border-style: double;
+  border-width: 15px;
+}
+
+p.six {
+  border-style: double;
+  border-width: thick;
+}
+</style>
+</head>
+<body>
+
+<h2>Vy Thành NAm </h2>
+<p>2021050469</p>
+
+<p class="one">Vy Thành NAm </p>
+<p class="two">Vy Thành NAm </p>
+<p class="three">2021050469</p>
+<p class="four">2021050469</p>
+<p class="five">Vy Thành NAm</p>
+<p class="six">SVy Thành NAm</p>
+
+<p><b>Note:</b> The "border-width" property does not work if it is used alone. 
+Always specify the "border-style" property to set the borders first.</p>
+
+</body>
+</html>
+
+
