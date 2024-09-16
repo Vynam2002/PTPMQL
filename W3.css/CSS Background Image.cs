5317bc3b75503fb6a71e35dd775e0b340fcc3f1c
Vynam2002 <vithanhnamckpt2002@gmail.com>
2024-09-16 22:40:14 +0700

    commit note

---
+++ W3.css/CSS Background Image.cs
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+  background-image: url("bgdesert.jpg");
+}
+</style>
+</head>
+<body>
+
+<h1>Vy Thành Nam </h1>
+<p>2021050469</p>
+
+</body>
+</html>
+
+
