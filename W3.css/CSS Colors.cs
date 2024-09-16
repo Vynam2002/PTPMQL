af0c6ff004ddf8372c62910ee323caa6593db38d
Vynam2002 <vithanhnamckpt2002@gmail.com>
2024-09-16 22:32:45 +0700

    commit note

---
+++ W3.css/CSS Colors.cs
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1 style="background-color:Tomato;">Vy thành nam </h1>
+<h1 style="background-color:Orange;">2021050469</h1>
+<h1 style="background-color:DodgerBlue;">Vy thành nam </h1>
+<h1 style="background-color:MediumSeaGreen;">2021050469</h1>
+<h1 style="background-color:Gray;">Vy thành nam </h1>
+<h1 style="background-color:SlateBlue;">Vy Thành Nam </h1>
+<h1 style="background-color:Violet;">Màu Hồng </h1>
+<h1 style="background-color:LightGray;">2021050469</h1>
+
+</body>
+</html>
+
