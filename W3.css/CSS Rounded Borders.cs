ecde1c84387636a531579e8123791f7197a032d3
Vynam2002 <vithanhnamckpt2002@gmail.com>
2024-09-16 22:59:13 +0700

    Commit note

---
+++ W3.css/CSS Rounded Borders.cs
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+p.normal {
+  border: 2px solid red;
+  padding: 5px;
+}
+
+p.round1 {
+  border: 2px solid red;
+  border-radius: 5px;
+  padding: 5px;
+}
+
+p.round2 {
+  border: 2px solid red;
+  border-radius: 8px;
+  padding: 5px;
+}
+
+p.round3 {
+  border: 2px solid red;
+  border-radius: 12px;
+  padding: 5px;
+}
+</style>
+</head>
+<body>
+
+<h2>Vy thành nam </h2>
+<p>2021050469</p>
+
+<p class="normal">Vy thành nam </p>
+<p class="round1">Vy thành nam </p>
+<p class="round2">Vy thành nam </p>
+<p class="round3">2021050469</p>
+
+</body>
+</html>
+
+
