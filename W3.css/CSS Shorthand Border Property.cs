5bdc93722a84f146833c2172afadc9d4bcbb72f0
Vynam2002 <vithanhnamckpt2002@gmail.com>
2024-09-16 22:57:49 +0700

    Commit note

---
+++ W3.css/CSS Shorthand Border Property.cs
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+p {
+  border-left: 6px solid red;
+  background-color: lightgrey;
+}
+</style>
+</head>
+<body>
+
+<h2>Vy thành nam </h2>
+<p>Vy thành nam  for border-left-width, border-left-style, and border-left-color.</p>
+
+</body>
+</html>
+
+
