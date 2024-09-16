85a7c03b71f04c2dff9cceb6d6d1315266052b63
Vynam2002 <vithanhnamckpt2002@gmail.com>
2024-09-16 22:45:03 +0700

    commit note

---
+++ W3.css/CSS Background Attachment.cs
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+  background-image: url("img_tree.png");
+  background-repeat: no-repeat;
+  background-position: right top;
+  margin-right: 200px;
+  background-attachment: scroll;
+}
+</style>
+</head>
+<body>
+
+<h1>Vy thành nam </h1>
+
+<p>2021050469</p>
+
+<p><strong>Tip:</strong> Vy thành nam , try 2021050469</p>
+
+<p>Vy thành nam </p>
+<p>2021050469</p>
+<p>Vy Thành Nam </p>
+<p>Vy thành nam </p>
+<p>2021050469</p>
+<p>Vy thành nam </p>
+<p>2021050469</p>
+
+
+</body>
+</html>
+
+
