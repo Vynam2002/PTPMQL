9c5907c63af5fd3a7cb40340dc3a9ba38def2277
Vynam2002 <vithanhnamckpt2002@gmail.com>
2024-09-16 22:42:27 +0700

    commit note

---
+++ W3.css/CSS Background Image Repeat.cs
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+  background-image: url("gradient_bg.png");
+  background-repeat: repeat-x;
+}
+</style>
+</head>
+<body>
+
+<h1>Vy thành nam </h1>
+<p>2021050469</p>
+
+</body>
+</html>
+
+
