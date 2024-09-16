5b25b5162a2a7dc04a7d5f5b755224986c3c86c9
Vynam2002 <vithanhnamckpt2002@gmail.com>
2024-09-16 22:56:43 +0700

    commit note

---
+++ W3.css/CSS Border Sides.cs
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+p {
+  border-top-style: dotted;
+  border-right-style: solid;
+  border-bottom-style: dotted;
+  border-left-style: solid;
+}
+</style>
+</head>
+<body>
+
+<h2>Vy thành nam </h2>
+<p>2021050469</p>
+
+</body>
+</html>
